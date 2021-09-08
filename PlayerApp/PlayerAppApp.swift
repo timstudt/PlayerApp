3791468c12ba4bd1699f41047fd85003faef4938
Tim Studt-Kristiansen <tstu@nuuday.dk>
2021-09-08 19:55:37 +0200

    Initial Commit

---
+++ PlayerApp/PlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerAppApp.swift
+//  PlayerApp
+//
+//  Created by Tim Studt-Kristiansen on 08/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct PlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
